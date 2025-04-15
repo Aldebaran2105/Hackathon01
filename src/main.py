fa677b96836db05e18b7fe4d115be7bb8804d857
Camila Espinoza <112716035+camilaeec@users.noreply.github.com>
2025-04-15 17:06:58 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,2 +1,10 @@
-def calculate() -> float:
-    pass
+def calculate(expression: str) -> float:
+
+
+
+
+
+    # Validar caracteres permitidos
+    for char in expression:
+        if not (char.isdigit() or char.isspace() or char in "+-*/()."):
+            raise ValueError(f"Carácter inválido encontrado: '{char}'")
